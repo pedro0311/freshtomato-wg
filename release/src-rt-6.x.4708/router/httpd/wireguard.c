ed5fdeaec9f8699cf367a59699cefb8fb5bf0263
Retrontology <retrontology@hotmail.com>
2023-07-12 01:57:56 -0500

    try reading file for wg status

--- release/src-rt-6.x.4708/router/httpd/wireguard.c
+++ release/src-rt-6.x.4708/router/httpd/wireguard.c
@@ -8,11 +8,29 @@ void asp_wg_status(int argc, char **argv)
 {
 	if (argc == 1)
 	{
+		FILE *fp;
 		char buffer[BUF_SIZE];
+		struct stat *st;
 
 		memset(buffer, 0, BUF_SIZE);
 		snprintf(buffer, BUF_SIZE, "/sys/class/net/%s/operstate", argv[0]);
 
-		web_printf("%d", eval("/bin/grep", "-xq", "'up\|unknown'", buffer) == 0);
+		int return_code = 0;
+
+		int err = stat(buffer, st);
+
+		if(err != -1) {
+
+			fp = fopen(buffer, "r");
+			getline(&buffer, BUF_SIZE, fp);
+			
+			if(strcmp(buffer, 'unknown') == 0 || strcmp(buffer, 'up') == 0)
+			{
+				return_code = 1;
+			}
+
+		}
+
+		web_printf("%d", return_code);
 	}
 }
