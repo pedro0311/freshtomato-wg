8db0840badba61cc3f9ffc2daf3a05b205512917
Retrontology <retrontology@hotmail.com>
2023-07-12 03:17:54 -0500

    debug logging

--- release/src-rt-6.x.4708/router/httpd/wireguard.c
+++ release/src-rt-6.x.4708/router/httpd/wireguard.c
@@ -1,6 +1,9 @@
 #include "tomato.h"
 
-#define BUF_SIZE		256
+#define LOGMSG_DISABLE	DISABLE_SYSLOG_OSM
+#define LOGMSG_NVDEBUG	"wg_debug"
+
+#define BUF_SIZE		64
 #define IF_SIZE			8
 
 
@@ -20,14 +23,16 @@ void asp_wg_status(int argc, char **argv)
 		int err = stat(buffer, &st);
 
 		if(err != -1) {
-
+			logmsg(LOG_INFO, "***WG*** opening wireguard operstate at %s", buffer);
 			fp = fopen(buffer, "r");
 			fgets(buffer, BUF_SIZE, fp);
-			
-			if(strcmp(buffer, 'unknown') == 0 || strcmp(buffer, 'up') == 0)
+			logmsg(LOG_INFO, "***WG*** found wireguard operstate: %s", buffer);
+			if(strncmp_ex(buffer, 'unknown') == 0 || strncmp_ex(buffer, 'up') == 0)
 			{
 				return_code = 1;
 			}
+			logmsg(LOG_INFO, "***WG*** closing wireguard operstate at %s", buffer);
+			fclose(fp);
 
 		}
 
