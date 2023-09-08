e99e8b6d519fc7ecdb9bb47519501b225aa9ac88
Retrontology <retrontology@hotmail.com>
2023-09-08 05:08:20 -0500

    rework usage

--- release/src-rt-6.x.4708/router/wireguard-tools/src/wg-quick/posix.sh
+++ release/src-rt-6.x.4708/router/wireguard-tools/src/wg-quick/posix.sh
@@ -247,7 +247,7 @@ parse_options() {
   ! expr match "${CONFIG_FILE}" '[a-zA-Z0-9_=+.-]\{1,15\}$' >/dev/null ||
     CONFIG_FILE="${CONFIG_FILE_BASE}/${CONFIG_FILE}.conf"
   [ -e "${CONFIG_FILE}" ] ||
-    die "\`${CONFIG_FILE}' does not exist"
+    die "'${CONFIG_FILE}' does not exist"
   #shellcheck disable=SC2003
   expr match "${CONFIG_FILE}" '\(.*/\)\?\([a-zA-Z0-9_=+.-]\{1,15\}\)\.conf$' >/dev/null ||
     die 'The config file must be a valid interface name, followed by .conf'
@@ -339,7 +339,7 @@ read_bool() {
   case "${2}" in
     true) eval "${1}=1" ;;
     false) eval "${1}=0" ;;
-    *) die "\`${2}' is neither true nor false" ;;
+    *) die "'${2}' is neither true nor false" ;;
   esac
 }
 
@@ -733,33 +733,33 @@ execute_hooks() {
 }
 
 cmd_usage() {
-	cat >&2 <<-_EOF
-	Usage: $PROGRAM [ up | down | save | strip ] [ CONFIG_FILE | INTERFACE ]
-	  CONFIG_FILE is a configuration file, whose filename is the interface name
-	  followed by \`.conf'. Otherwise, INTERFACE is an interface name, with
-	  configuration found at /etc/wireguard/INTERFACE.conf. It is to be readable
-	  by wg(8)'s \`setconf' sub-command, with the exception of the following additions
-	  to the [Interface] section, which are handled by $PROGRAM:
-	  - Address: may be specified one or more times and contains one or more
-	    IP addresses (with an optional CIDR mask) to be set for the interface.
-	  - DNS: an optional DNS server to use while the device is up.
-	  - MTU: an optional MTU for the interface; if unspecified, auto-calculated.
-	  - Table: an optional routing table to which routes will be added; if
-	    unspecified or \`auto', the default table is used. If \`off', no routes
-	    are added.
-	  - PreUp, PostUp, PreDown, PostDown: script snippets which will be executed
-	    by bash(1) at the corresponding phases of the link, most commonly used
-	    to configure DNS. The string \`%i' is expanded to INTERFACE.
-	  - SaveConfig: if set to \`true', the configuration is saved from the current
-	    state of the interface upon shutdown.
-	See wg-quick(8) for more info and examples.
-	_EOF
+
+	echo "Usage: $PROGRAM [ up | down | save | strip ] [ CONFIG_FILE | INTERFACE ]"
+	echo "  CONFIG_FILE is a configuration file, whose filename is the interface name"
+	echo "  followed by '.conf'. Otherwise, INTERFACE is an interface name, with"
+	echo "  configuration found at /etc/wireguard/INTERFACE.conf. It is to be readable"
+	echo "  by wg(8)'s 'setconf' sub-command, with the exception of the following additions"
+	echo "  to the [Interface] section, which are handled by $PROGRAM:"
+	echo "  - Address: may be specified one or more times and contains one or more"
+	echo "    IP addresses (with an optional CIDR mask) to be set for the interface."
+	echo "  - DNS: an optional DNS server to use while the device is up."
+	echo "  - MTU: an optional MTU for the interface; if unspecified, auto-calculated."
+	echo "  - Table: an optional routing table to which routes will be added; if"
+	echo "    unspecified or 'auto', the default table is used. If 'off', no routes"
+	echo "    are added."
+	echo "  - PreUp, PostUp, PreDown, PostDown: script snippets which will be executed"
+	echo "    by bash(1) at the corresponding phases of the link, most commonly used"
+	echo "    to configure DNS. The string '%i' is expanded to INTERFACE."
+	echo "  - SaveConfig: if set to 'true', the configuration is saved from the current"
+	echo "    state of the interface upon shutdown."
+	echo "See wg-quick(8) for more info and examples."
+
 }
 
 cmd_up() {
   i=''
   [ -z "$(ip link show dev "${INTERFACE}" 2>/dev/null)" ] ||
-    die "\`${INTERFACE}' already exists"
+    die "'${INTERFACE}' already exists"
   exit_trap push 'del_if'
   eval "execute_hooks ${PRE_UP}"
   add_if
@@ -792,7 +792,7 @@ cmd_up() {
 cmd_down() {
   case " $(wg show interfaces) " in
     *" ${INTERFACE} "*) : ;;
-    *) die "\`${INTERFACE}' is not a WireGuard interface" ;;
+    *) die "'${INTERFACE}' is not a WireGuard interface" ;;
   esac
   eval "execute_hooks ${PRE_DOWN}"
   [ "${SAVE_CONFIG}" -eq 0 ] ||
@@ -806,7 +806,7 @@ cmd_down() {
 cmd_save() {
   case " $(wg show interfaces) " in
     *" ${INTERFACE} "*) : ;;
-    *) die "\`${INTERFACE}' is not a WireGuard interface" ;;
+    *) die "'${INTERFACE}' is not a WireGuard interface" ;;
   esac
   save_config
 }
