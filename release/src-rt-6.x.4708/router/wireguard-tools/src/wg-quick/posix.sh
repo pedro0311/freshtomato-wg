1fcb8244356e07129f8d71b41e2261a2bc15476f
Retrontology <retrontology@hotmail.com>
2023-09-08 05:14:22 -0500

    use better monkeypatch

--- release/src-rt-6.x.4708/router/wireguard-tools/src/wg-quick/posix.sh
+++ release/src-rt-6.x.4708/router/wireguard-tools/src/wg-quick/posix.sh
@@ -37,8 +37,38 @@ exit_trap() {
 
 # freshtomato doesn't have readlink so we need to monkeypatch
 readlink() {
-  L=$(ls -dl ${1})
-  echo ${L#*-> } 
+  [ "${1:-}" ] || return 1
+  max_symlinks=40
+  CDPATH='' # to avoid changing to an unexpected directory
+
+  target=$1
+  [ -e "${target%/}" ] || target=${1%"${1##*[!/]}"} # trim trailing slashes
+  [ -d "${target:-/}" ] && target="$target/"
+
+  cd -P . 2>/dev/null || return 1
+  while [ "$max_symlinks" -ge 0 ] && max_symlinks=$((max_symlinks - 1)); do
+    if [ ! "$target" = "${target%/*}" ]; then
+      case $target in
+        /*) cd -P "${target%/*}/" 2>/dev/null || break ;;
+        *) cd -P "./${target%/*}" 2>/dev/null || break ;;
+      esac
+      target=${target##*/}
+    fi
+
+    if [ ! -L "$target" ]; then
+      target="${PWD%/}${target:+/}${target}"
+      printf '%s\n' "${target:-/}"
+      return 0
+    fi
+
+    # `ls -dl` format: "%s %u %s %s %u %s %s -> %s\n",
+    #   <file mode>, <number of links>, <owner name>, <group name>,
+    #   <size>, <date and time>, <pathname of link>, <contents of link>
+    # https://pubs.opengroup.org/onlinepubs/9699919799/utilities/ls.html
+    link=$(ls -dl -- "$target" 2>/dev/null) || break
+    target=${link#*" $target -> "}
+  done
+  return 1
 }
 
 # embedded systems without char-classes in "tr" need monkeypatching
